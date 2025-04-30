89790d9713452d00b9ed0df8b7ef9b23db565b27
Borislav Kozhuharski <nomercyforyouss@gmail.com>
2025-04-30 15:55:02 +0200

    first commit

---
+++ pixel.js
@@ -0,0 +1,6 @@
+const API_KEY = '5wkRzl5NJbSpAib6zTIdX7Z0COZe1lX9r8vvtbWgETsYhPtlTpCLdeuQ'
+
+const loadImage = (query = 'nature') => {
+    fetch('https://api.pexels.com/v1/search?query=[your-query]')
+    headers:
+}
